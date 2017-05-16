7343348cac2baf286d7853fae0ac8f724b5755e7
User <User@user.com>
2017-05-16 11:18:18 +0200

    -adding windows automatic path setting batch file

---
+++ hotcrp-docker/fixwindowsmount.bat
@@ -0,0 +1,17 @@
+@echo off
+pushd \
+%~d0
+cd "%~dp0"
+set vboxmanexe="%ProgramFiles%\Oracle\VirtualBox\VBoxManage.exe"
+call :NORMALIZEPATH "..\"
+set hcrplocpath=%RETVAL:~0,-1%
+docker-machine stop default
+%vboxmanexe% controlvm default poweroff soft
+%vboxmanexe% sharedfolder add default --name hcrp0 --hostpath "%hcrplocpath%" --automount
+
+popd
+
+EXIT /B
+:NORMALIZEPATH
+  SET RETVAL=%~dpfn1
+  EXIT /B
